d1deb99c67ad3f1e27f81a8dbee49c269d4b0542
NumHum13 <45885058+NumHum13@users.noreply.github.com>
2019-06-21 09:15:34 -0700

    Add files via upload

---
+++ KeyInput.java
@@ -0,0 +1,34 @@
+package minePackage;
+//import java.awt.*;  
+import java.awt.event.*; 
+import java.awt.event.KeyListener;
+
+import javax.swing.SwingUtilities;
+
+public class KeyInput  implements KeyListener{
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		Ground game = MineSweeper.game;
+		//System.out.println(e.getKeyCode());
+		if(e.getKeyCode() == 16)
+		{
+			System.out.println("Shift Clicked");
+			game.shiftHeld(true);
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
